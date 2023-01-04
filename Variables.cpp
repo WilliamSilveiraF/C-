f6e32f95bbe9e3749e2391bac0bceaa530b70083
williamsilveiraf <williamsilveiraf@hotmail.com>
2023-01-03 23:35:26 -0300

    feat: DrawingShape, HelloWorld, Variables

---
+++ Variables.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string charName = "John Doe";
+    
+    int charAge;
+    charAge = 35;
+
+    cout << "There once was a man named " << charName << endl;
+    cout << "He was " << charAge << " years old" << endl;
+    cout << "He liked the name " << charName << endl;
+    cout << "But did not like being " << charAge << endl;
+
+    return 0;
+}
